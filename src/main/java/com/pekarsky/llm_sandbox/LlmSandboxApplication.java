0927db7e5be9dd7455bf9d013c166f5253dfad0b
Serge Pekarsky <serge.pekarsky@gmail.com>
2025-03-22 12:58:15 +0100

    spring boot (+ai)  support

---
+++ src/main/java/com/pekarsky/llm_sandbox/LlmSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.pekarsky.llm_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pekarsky/llm_sandbox/LlmSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pekarsky.llm_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
